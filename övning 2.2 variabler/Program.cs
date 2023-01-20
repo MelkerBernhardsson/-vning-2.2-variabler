eb5cd336f1ac61ccaa61fb095493c74f3e1e55f1
Pingvu <nhlpool27@gmail.com>
2023-01-20 09:23:42 +0100

    Add project files.

---
+++ övning 2.2 variabler/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace uppgift2
+{
+    class program
+    {
+        static void Main(string[]args)
+        {
+            Console.WriteLine("Vilket årtal är du född");
+            string födelsedagtext = Console.ReadLine();
+            int födelsedag = int.Parse(födelsedagtext);
+            int gammal = 2023-födelsedag;
+            Console.WriteLine("du är född år " + födelsedag + " du är eller kommer att bli " + gammal + " i år"); 
+
+        }
+    }
+}
